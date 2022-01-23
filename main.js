fa7fe0a7e27045b8bb998492393601d86b39d72b
rakibul-cse-diu <rakibul2180@gmail.com>
2022-01-23 11:14:08 +0600

    responsiveness added

---
+++ main.js
@@ -0,0 +1,14 @@
+const Menu = document.getElementById("menu");
+const Nav = document.getElementById("nav");
+const ToggleClose = document.getElementById("toggle-close");
+
+
+const Open = () => {
+    Nav.style.right = "0";
+    Menu.style.display = "none";
+}
+
+const Close = () => {
+    Nav.style.right = "-427px";
+    Menu.style.display = "block"
+}
